e7e51ce527770b08e8eb61028aaf9205324131b5
Javiero9000 <elavat.9000@gmail.com>
2025-06-24 20:01:33 -0600

    Crea pagina de logeo y cambia estilos.

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -1,48 +1,27 @@
 <div class="login-box">
   <div class="login-logo">
-    <img src="vistas/Img/plantilla/Logo-plano.png"  class="img-responsive" style="padding: 10px 10px 0px 10px;">
+    <img src="vistas/Img/plantilla/Logo-plano.png" class="img-responsive" style="padding: 10px 10px 0px 10px;">
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+    <p class="login-box-msg">Ingresar al sistema</p>
 
-    <form action="../../index2.html" method="post">
+    <form method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Usuario" name="ingUsuario" required>
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
+
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Contraseña" name="ingPassword" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
+      
+      <div class="row"> <!-- Añade esta fila -->
+        <div class="col-xs-4 col-xs-offset-4"> <!-- Añade offset para centrar -->
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
       </div>
     </form>
-
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
-
   </div>
-  <!-- /.login-box-body -->
 </div>
