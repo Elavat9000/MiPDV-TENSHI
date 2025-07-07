6025f61ba85d4744b0093dda7a1010d4e144fc05
Javiero9000 <elavat.9000@gmail.com>
2025-07-07 15:59:40 -0600

    Verifica el que no exista fallos en las fotos modificadas en los usuarios.

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -215,7 +215,7 @@ class ControladorUsuarios{
                 
                 $ruta = $_POST['fotoActual'];
 
-                if(isset($_FILES['editarFoto']["tmp_name"])) {
+                if(isset($_FILES['editarFoto']["tmp_name"]) && !empty($_FILES['editarFoto']["tmp_name"])) {
 
                     list($ancho,$alto) = getimagesize($_FILES["editarFoto"]["tmp_name"]);
 
